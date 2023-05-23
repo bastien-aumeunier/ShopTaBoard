f7ce284cd0313254d502da7eaf019641c7dedbbb
Bastien Aumeunier <bastien.aumeunier@ynov.com>
2023-05-23 12:54:59 +0200

    remove useful code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { createRoot } from "react-dom/client";
 import router from './Utils/route.utils'
-import { atom } from 'jotai'
-const JWTAtom = atom('')
+
 
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
